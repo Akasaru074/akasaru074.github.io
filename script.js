ec493f9c983fd11094102c4554bbac8df17ca985
BuildTools <unconfigured@null.spigotmc.org>
2022-11-08 13:51:59 +1000

    ver 1.0

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 const list = document.querySelector(".list");
 const input = document.querySelector(".inp");
 const button = document.querySelector(".butt");
+const triesEl = document.querySelector(".tries");
+
+const TRIES = 3;
 
 const words = [
-    {word: "ананас", indexes:[0, 5]}
+    {text: "квас", indexes:[0]},
+    {text: "дорога", indexes:[0, 4]}
 ];
 
 let word = words[Math.floor(Math.random() * words.length)];
 
-for (let i = 0; i < word.word.length; i++) {
+for (let i = 0; i < word.text.length; i++) {
     let elem = document.createElement("li");
-    word.indexes.includes(i) ? elem.innerText = word.word[i] : elem.innerText = "_";
+    word.indexes.includes(i) ? elem.innerText = word.text[i] : elem.innerText = "_";
     list.appendChild(elem);
 }
+triesEl.textContent += TRIES;
+
+(()=>{
+    let tries = TRIES;
 
 button.addEventListener("click", ()=>{
     let answer = input.value.toLowerCase();
     let lis = document.getElementsByTagName("li");
-    if(word.word.includes(answer)) {
-        if(word.word.indexOf(answer) != word.word.lastIndexOf(answer)) 
-            for (let i = word.word.indexOf(answer); i < word.word.lastIndexOf(answer); i++) {
-                if(word.word[i] == answer && lis[i] == "_") lis[word.word.split("").indexOf(answer)].innerText = answer;
+    let hasFound = false;
+    input.value = "";
+    for (let i = 0; i < word.text.length; i++) {
+        if(word.text[i] == answer && !word.indexes.includes(i)) {
+            lis[i].innerText = answer;
+            word.indexes.push(i);
+            if(word.indexes.length == word.text.length) {
+                const el = document.createElement("h1");
+                el.textContent = "Ты выйграл!";
+                document.body.textContent = "";
+                document.body.appendChild(el);
+                return;
             }
-        else lis[word.word.split("").indexOf(answer)].innerText = answer;
+            console.log("успешно");
+            hasFound = true;
+            continue;
+        }
+        if(i == word.text.length - 1 && !hasFound) {
+            tries--;
+            triesEl.textContent = "Оставшиеся попытки: " + tries;
+            if (tries <= 0) {
+                document.body.innerText = "Ты проиграл, перезагрузи страницу!";
+            }
+            console.log("неудачно");
+            console.log("оставшеися попытки: " + tries);
+        }
     }
 });
+
+})()
